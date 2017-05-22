eae8566721c7f8c56d2a955af31a3c984b12bd35
SterOtto <SterOtto@outlook.com>
2017-05-22 21:14:29 +0800

    增加ckeditor上传图片

---
+++ src/main/java/com/green/lights/config/PublicResourceConfiguration.java
@@ -0,0 +1,22 @@
+package com.green.lights.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by SterOtto on 2017/5/22.
+ */
+@Configuration
+public class PublicResourceConfiguration extends WebMvcConfigurerAdapter {
+
+    @Value("${application.storage.path.base}")
+    private String resourceLocation;
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/public/files/**")
+            .addResourceLocations("file:" + resourceLocation);
+        super.addResourceHandlers(registry);
+    }
+}
